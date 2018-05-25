6d2dff304a6c51913cf43df55b1c47b8720f7abc
Me-Yan <yanyanghong_work@163.com>
2018-05-25 18:13:18 +0800

    add code

--- src/app/user-add/user-add.component.ts
+++ src/app/user-add/user-add.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { UserService } from '../service/user.service';
+import { UserModel } from '../model/user.model';
 
 @Component({
   selector: 'app-user-add',
@@ -8,7 +10,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class UserAddComponent implements OnInit {
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private userService: UserService) { }
 
   display;  // control the modal
 
@@ -21,18 +23,19 @@ export class UserAddComponent implements OnInit {
         [
           Validators.required,
           Validators.minLength(2),
-          Validators.maxLength(16)
+          Validators.maxLength(16),
+          this.validateNameIsExist
         ]
       ],
       age: [
         '',
         [
           Validators.required,
-          Validators.pattern('/^([0-9]|([1-9][0-9])|([1-9][0-9]{2}))$/')
+          Validators.pattern('^([0-9]|[1-9][0-9]|1[0-9]{2})$')
         ]
       ],
       sex: [
-        '',
+        '男',
         [
           Validators.required
         ]
@@ -60,6 +63,44 @@ export class UserAddComponent implements OnInit {
       }
     } else {
       this.display = event;
+    }
+  }
+
+  validateNameIsExist(formControl: FormControl) {
+    const nameField = formControl.value;
+    if (!nameField) {
+      return {
+        validateNameIsExist: {
+          valid: true
+        }
+      };
+    }
+
+    let userInfoList: UserModel[];
+    this.userService.listUser().subscribe(data => {
+      userInfoList = data;
+    });
+
+    let flag = false;
+    for (const userItem of userInfoList) {
+      if (nameField === userItem.name) {
+        flag = true;
+        break;
+      }
+    }
+
+    if (flag) {
+      return {
+        validateNameIsExist: {
+          valid: false
+        }
+      };
+    } else {
+      return {
+        validateNameIsExist: {
+          valid: true
+        }
+      };
     }
   }
 
